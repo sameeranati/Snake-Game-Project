ada2c810625ffb873b6b51abd1d1ab6de2146d73
sameeranati <113128931+sameeranati@users.noreply.github.com>
2023-02-15 15:24:40 -0500

    Add files via upload

---
+++ main.py
@@ -0,0 +1,15 @@
+with open("/home/sameeranati/my_file.txt", mode='r') as file:
+    content=file.read("i m a very good girl")
+    print(content)
+# with open("new_file.txt",mode="w") as file:
+#     file.write("I am a very good girl")
+
+# from pathlib import Path
+# print(Path.cwd())
+
+# import os
+ 
+# x=os.path.basename('my_file.txt')
+# print(x)
+# y=os.path.dirname('my_file.txt')
+# print(y)
